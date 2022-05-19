724fe0b6a2c387bb3798cca99c79c3e3fe424754
ohampton <44102431+ohampton@users.noreply.github.com>
2022-05-19 12:01:50 -0400

    resetSubmit

--- client/src/components/contact/Contact.jsx
+++ client/src/components/contact/Contact.jsx
@@ -16,11 +16,8 @@ const Contact = () => {
     e.preventDefault();
 
     emailjs.sendForm('service_1lrgkja', 'template_ncgqhkp', form.current, 'RUcasiQtqQWoueWGK')
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      });
+
+    e.target.reset()
   };
 
   return (
