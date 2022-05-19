060776714726586f5b6b2082700ffdd9f1db53c5
ohampton <44102431+ohampton@users.noreply.github.com>
2022-05-19 18:47:52 -0400

    portfolioUpdate

--- client/src/components/portfolio/Portfolio.jsx
+++ client/src/components/portfolio/Portfolio.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './portfolio.css'
-import IMG1 from '../../assets/portfolio1.jpg'
+import IMG1 from '../../assets/oscarDance.jpeg'
 import IMG2 from '../../assets/portfolio2.jpg'
 import IMG3 from '../../assets/steph3.jpeg'
 import IMG4 from '../../assets/portfolio4.jpg'
@@ -12,8 +12,8 @@ const data = [
     id: 1,
     image: IMG1,
     title: 'Oscar Dance Site',
-    github: 'https//github.com',
-    demo: 'https//github.com'
+    github: 'https://ohampton.github.io/myPortfolio/',
+    demo: 'https://ohampton.github.io/myPortfolio/'
   },
   {
     id: 2,
