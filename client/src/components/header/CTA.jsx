98086c9dd2fe87792cc0f9b3d5c5206fcd0b70b8
ohampton <44102431+ohampton@users.noreply.github.com>
2022-05-19 12:53:35 -0400

    resumefix

--- client/src/components/header/CTA.jsx
+++ client/src/components/header/CTA.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-// import Resume from '../../../assets/'
+import Resume from '../../assets/OzCV.pdf'
 
 const CTA = () => {
   return (
     <div className="cta">
-        <a href="https//github.com" className='btn'>Download Resume</a>
+        <a href={Resume} className='btn'>Download Resume</a>
         <a href="#contact" className='btn btn-primary'>Contact Me</a>
     </div>
   )
